16a5d254b8c0095d91ab1e3a80d45b490eb3f2b3
Rareș Vasiu <72668860+Rares-AndreiVasiu@users.noreply.github.com>
2024-02-24 16:50:28 +0200

    Florian read from file example

--- lab1/main.java
+++ lab1/main.java
@@ -1,1 +1,37 @@
-//demo file
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+class ReadFile {
+  public static void main(String[] args) {
+    try {
+      File file_to_be_read = new File("read_txt.txt");
+      Scanner read_file = new Scanner(file_to_be_read);
+      while (read_file.hasNextLine()) {
+        String data = read_file.nextLine();
+        System.out.println(data);
+      }
+      read_file.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
+
+class ReadFileCmd {
+  public static void main(String[] args) {
+    try {
+      File read_the_filename = new File(args[0]);
+      Scanner read_from_filename = new Scanner(read_the_filename);
+      while (read_from_filename.hasNextLine()) {
+        String data = read_from_filename.nextLine();
+        System.out.println(data);
+      }
+      read_from_filename.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
+}
